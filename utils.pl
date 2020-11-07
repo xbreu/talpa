dbac2c584329e68fbc61f2ace8db60fbcd2c85d5
Jumaruba <juliane.marubayashi@gmail.com>
2020-11-07 19:00:26 +0000

    valid_moves almost done

--- utils.pl
+++ utils.pl
@@ -2,17 +2,14 @@
  
 
 
-getBoardNumCols([X|_], Cols):- 
+getBoardNumCols([X|_], Cols):- !,
 	length(X, Cols).  
 
-getBoardNumLines([X|GameState], Lines):-  
+getBoardNumLines([X|GameState], Lines):-  !,
 	length([X|GameState], Lines). 
 
-getValueInMatrix(GameState, Line, Col, Value):-   
+getValueInMatrix(GameState, Line, Col, Value):- !,
 	nth0(Line, GameState, L), 
 	nth0(Col, L, Value).
 
-possible_letters_move('w'). 
-possible_letters_move('a').
-possible_letters_move('s').
-possible_letters_move('d').   
+	   
