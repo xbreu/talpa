544a6da6f6a4c52fd21f0157472f87e3b6d7375b
Jumaruba <juliane.marubayashi@gmail.com>
2020-11-13 12:59:43 +0000

    length col

--- bot.pl
+++ bot.pl
@@ -45,6 +45,44 @@ orthogonal_row_length([_, _], _, _, NumCells, NumCells).
 
 
 
+/**
+ Get's the Vertical length of a path. 
+
+ orthogonal_col_length([+StartCol, +StartLine], +Board, -NumCells).
+ +StartCol      : Col where the path starts. 
+ +StartLine     : Line where the path starts. 
+ +Board         : GameState. 
+ -NumCells      : Vertical length in cells measure. 
+*/ 
+orthogonal_col_length([StartCol, StartLine], Board, NumCells):-
+        can_visit_adjacent([StartCol, StartLine], [], Board),
+        orthogonal_col_length([StartCol, StartLine], Board, [], NumCells, 1), !. 
+
+% If the first cell is not 0, e cannot visit it. So the NumCells is 0. 
+orthogonal_col_length([CurrCol, CurrLine], Board, 0):-                  
+        \+can_visit_adjacent([CurrCol, CurrLine], [], Board). 
+
+     
+% The next cell we can visit adds an horizontal cell. 
+orthogonal_col_length([CurrCol, CurrLine], Board, Visited, NumCells, AccNumCells):-
+        adjacent_cell([CurrCol, CurrLine], [NewCol, NewLine]),
+        can_visit_adjacent([NewCol, NewLine], Visited, Board),          
+        is_adjacent_in_col([CurrCol, CurrLine], [NewCol, NewLine]), !,  
+        NewAccNumCells is AccNumCells +1, 
+        orthogonal_col_length([NewCol, NewLine], Board, [[CurrCol, CurrLine]|Visited], NumCells, NewAccNumCells).  
+                    
+% The next cell we can visit doesn't add an horizontal cell. 
+orthogonal_col_length([CurrCol, CurrLine], Board, Visited, NumCells, AccNumCells):-
+        adjacent_cell([CurrCol, CurrLine], [NewCol, NewLine]),
+        can_visit_adjacent([NewCol, NewLine], Visited, Board),          
+        \+is_adjacent_in_col([CurrCol, CurrLine], [NewCol, NewLine]), !,  
+        orthogonal_col_length([NewCol, NewLine], Board, [[CurrCol, CurrLine]|Visited], NumCells, AccNumCells).  
+
+% If no other cell can be visit, ends the recursion. 
+orthogonal_col_length([_, _], _, _, NumCells, NumCells). 
+
+
+
 
 can_visit_adjacent([CurrCol, CurrLine], Visited, Board):-
         \+member([CurrCol, CurrLine], Visited),                                  % Isnt visited yet 
