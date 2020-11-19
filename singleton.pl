d2da4e106fc9ed5643c0efcbe15d5989ee2b9d8c
Alexandre Abreu <up201800168@up.pt>
2020-11-19 15:55:07 +0000

    singleton comments

--- singleton.pl
+++ singleton.pl
@@ -1,14 +1,24 @@
+% -----------------------------------------------
+% Board variables
+% -----------------------------------------------
+
 % Determines the number of lines in the game.
 numberOfLines(3).
 
 % Determines the number of columns in the game. 
 numberOfCols(3).
 
+% -----------------------------------------------
+% Players variables
+% -----------------------------------------------
+
 % horizontal and vertical players
 vertical_player(1).
 horizontal_player(2).
 
-% player codes
+% empty cell code
 code(0,' ').
+
+% player codes
 code(1,'\x00445\').
 code(2,'\x25CF\').
