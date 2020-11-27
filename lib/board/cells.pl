84d832729e2d72c57f26121a3b194f91ebf7222a
a3brx <a3brx@sapo.pt>
2020-11-27 09:13:08 +0000

    modularization

--- lib/board/cells.pl
+++ lib/board/cells.pl
@@ -1,30 +1,5 @@
 :- use_module(library(lists)).
-:- use_module(library(random)).
-:- consult('singleton.pl').
-
-% -----------------------------------------------
-% List functions
-% -----------------------------------------------
-
-% Calculates length of list
-len([],0).
-len([_|T],N) :-
-	len(T,A),
-	N is A + 1.
-
-% Chooses a random element from a list.
-% random_list(+List, -Element).
-random_list(List, Element) :-
-        len(List, X), 
-        random(0, X, R), 
-        nth0(R, List, Element).
-
-% Sum of all elements in a list.
-% sumList(+List, -Sum).
-sumList([], 0).
-sumList([X|L], Sum) :-
-	sumList(L, NewSum), 
-	Sum is NewSum + X.
+:- consult('../variables.pl').
 
 % -----------------------------------------------
 % Matrix functions
@@ -43,12 +18,12 @@ empty_cell(Board, Col-Row) :-
 % +Col			: Column to get the value.
 % -Value		: The value of the cell in the +Line, +Col position.
 getValueInMatrix(GameState, Line, Col, Value):- !,
-	nth0(Line, GameState, L), 
+	nth0(Line, GameState, L),
 	nth0(Col, L, Value).
 
-% ----------------------------------------------- 
+% -----------------------------------------------
 % Replace functions
-% ----------------------------------------------- 
+% -----------------------------------------------
 
 % Replaces an element from +Matrix in +Position for a new value called +NewValue.
 % replaceInMatrix(+Matrix, +Position, +NewValue, -NewMatrix).
@@ -56,10 +31,10 @@ getValueInMatrix(GameState, Line, Col, Value):- !,
 % +Position 	: Position in +List to change the value.
 % +NewValue 	: The new value of the element in List[Position].
 % -NewMatrix	: List to be returned.
-replaceInMatrix([L|GameState], 0-Col, NewValue, [NewL|GameState]):- !, 
+replaceInMatrix([L|GameState], 0-Col, NewValue, [NewL|GameState]):- !,
 	replaceInList(L, Col, NewValue, NewL).
-replaceInMatrix([L|GameState], Line-Col, NewValue, [L|NewGameState]):- !, 
-	NewLine is Line -1,  
+replaceInMatrix([L|GameState], Line-Col, NewValue, [L|NewGameState]):- !,
+	NewLine is Line -1,
 	NewLine >= 0,
 	replaceInMatrix(GameState, NewLine-Col, NewValue, NewGameState).
 
@@ -71,9 +46,9 @@ replaceInMatrix([L|GameState], Line-Col, NewValue, [L|NewGameState]):- !,
 % -NewList 	: List to be returned.
 replaceInList([_|List], 0, NewValue, [NewValue|List]).
 replaceInList([L|List], Pos, NewValue, [L|NewList]):- !,
-	AuxPos is Pos- 1, 
-	AuxPos >= 0, 
-	replaceInList(List, AuxPos, NewValue, NewList). 
+	AuxPos is Pos- 1,
+	AuxPos >= 0,
+	replaceInList(List, AuxPos, NewValue, NewList).
 
 % ----------------------------------------------------------------
 % Adjacency functions
@@ -81,12 +56,12 @@ replaceInList([L|List], Pos, NewValue, [L|NewList]):- !,
 
 % Returns true if the first list and the second are adjacent
 adjacent_cell([StartCol, StartRow], [StartCol, NextRow]) :-
-	NextRow is StartRow + 1, 
-	numberOfLines(Lines), 
+	NextRow is StartRow + 1,
+	numberOfLines(Lines),
 	NextRow < Lines.
 adjacent_cell([StartCol, StartRow], [NextCol, StartRow]) :-
 	NextCol is StartCol + 1,
-	numberOfCols(Cols),  
+	numberOfCols(Cols),
 	NextCol < Cols.
 adjacent_cell([StartCol, StartRow], [StartCol, PreviousRow]) :-
 	PreviousRow is StartRow - 1,
@@ -95,15 +70,14 @@ adjacent_cell([StartCol, StartRow], [PreviousCol, StartRow]) :-
 	PreviousCol is StartCol - 1,
 	PreviousCol >= 0.
 
-% Checks if two cells are adjacents in a row. 
+% Checks if two cells are adjacents in a row.
 is_adjacent_in_row([StartCol, StartRow], [NextCol, StartRow]):-
 	NextCol is StartCol +1, !.
 is_adjacent_in_row([StartCol, StartRow], [PreviousCol, StartRow]):-
 	PreviousCol is StartCol -1.
 
-% Cheks if two cells are adjacents in a col. 
-is_adjacent_in_col([StartCol, StartRow], [StartCol, NextRow]):- 
+% Cheks if two cells are adjacents in a col.
+is_adjacent_in_col([StartCol, StartRow], [StartCol, NextRow]):-
 	NextRow is StartRow +1, !.
 is_adjacent_in_col([StartCol, StartRow], [StartCol, PreviousRow]):-
-	PreviousRow is StartRow -1.  
-
+	PreviousRow is StartRow -1.
