9deaaa02f4eb1440f415f565266309904b688c36
Alexandre Abreu <up201800168@up.pt>
2020-11-27 09:13:08 +0000

    modularization

--- lib/variables.pl
+++ lib/variables.pl
@@ -5,14 +5,14 @@
 % Determines the number of lines in the game.
 numberOfLines(8).
 
-% Determines the number of columns in the game. 
+% Determines the number of columns in the game.
 numberOfCols(8).
 
 % Size of padding on the board
 padding_size(1).
 
 % -----------------------------------------------
-% Players variables
+% Player variables
 % -----------------------------------------------
 
 % horizontal and vertical players
@@ -26,15 +26,3 @@ code(0,' ') :- !.
 code(1,'\x00445\') :- !.
 code(2,'\x25CF\') :- !.
 code(X, X).
-
-
-game_over_1([[0,2,1,2,1,1,1,2],[0,1,2,1,1,0,0,2],[2,0,0,0,0,0,2,2],[0,0,2,0,1,0,0,0],[1,1,0,2,1,1,0,0],[2,0,1,0,2,1,0,2],[2,0,1,0,2,1,0,2],[1,2,1,1,1,2,2,2],[2,1,2,1,2,1,2,1]]).
-
-game_over_2([[1,1,0,1,0,0,0,2],
-[0,0,2,0,1,0,2,0],
-[1,0,0,0,1,1,0,1],
-[0,1,0,1,2,0,0,1],
-[1,0,0,0,0,0,0,0],
-[2,2,0,2,0,0,0,0],
-[2,2,0,0,0,0,0,0],
-[0,1,2,0,2,0,0,2]]).
