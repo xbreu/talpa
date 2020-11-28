6318b788de9fa49ba92ce9c0eb85025a1b752fa3
Alexandre Abreu <up201800168@up.pt>
2020-11-28 15:54:57 +0000

    portuguese working with accents

--- lib/text/translations.pl
+++ lib/text/translations.pl
@@ -19,13 +19,13 @@ indicateNotMemberString(en, 'Value must be one of ~w').
 % ----------------------------------------------------------------
 requestInputRowString(pt, 'Linha').
 requestInputColString(pt, 'Coluna').
-requestInputDirString(pt, 'Direção').
-requestIntervalOptionString(pt, 'Escolha uma opção [~d-~d]').
-requestLevelOptionString(pt, 'Escolha um nível [~d-~d] ou digite 0').
+requestInputDirString(pt, 'Dire\x00E7\\x00E3\o').
+requestIntervalOptionString(pt, 'Escolha uma op\x00E7\\x00E3\o [~d-~d]').
+requestLevelOptionString(pt, 'Escolha um n\x00ED\vel [~d-~d] ou digite 0').
 requestTryAgainString(pt, 'Tente Novamente').
 indicatePlayerTurnString(pt, 'Turno do jogador ~s').
-indicateInvalidPieceString(pt, 'Essa peça não é sua').
-indicateNoMovementsFromPieceString(pt, 'Não existem movimentos possíveis a partir desta peça').
-indicateNoMovesString(pt, 'Não existem capturas disponíveis, remova uma peça').
+indicateInvalidPieceString(pt, 'Essa pe\x00E7\a n\x00E3\o \x00E9\ sua').
+indicateNoMovementsFromPieceString(pt, 'N\x00E3\o existem movimentos poss\x00ED\veis a partir desta pe\x00E7\a').
+indicateNoMovesString(pt, 'N\x00E3\o existem capturas dispon\x00ED\veis, remova uma pe\x00E7\a').
 indicateInvalidDomainString(pt, 'O valor deve pertencer ao intervalo [~w-~w]').
 indicateNotMemberString(pt, 'O valor deve ser um de ~w').
