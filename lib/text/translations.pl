4d0291f3017837e1e44334786459cb0606dfbeac
a3brx <a3brx@sapo.pt>
2020-11-28 15:01:53 +0000

    added translations file

---
+++ lib/text/translations.pl
@@ -0,0 +1,29 @@
+% ----------------------------------------------------------------
+% English
+% ----------------------------------------------------------------
+requestInputRowString(en, 'Row').
+requestInputColString(en, 'Col').
+requestInputDirString(en, 'Direction').
+requestIntervalOptionString(en, 'Choose an option [~d-~d]').
+requestLevelOptionString(en, 'Choose a level [~d-~d] or type 0').
+requestTryAgainString(en, 'Try Again').
+indicatePlayerTurnString(en, 'Player ~s turn').
+indicateInvalidPieceString(en, 'That piece is not yours').
+indicateNoMovesString(en, 'No captures available, remove a piece').
+indicateInvalidDomainString(en, 'Value must be between [~w-~w]').
+indicateNotMemberString(en, 'Value must be one of ~w').
+
+% ----------------------------------------------------------------
+% Português
+% ----------------------------------------------------------------
+requestInputRowString(pt, 'Linha').
+requestInputColString(pt, 'Coluna').
+requestInputDirString(pt, 'Direção').
+requestIntervalOptionString(pt, 'Escolha uma opção [~d-~d]').
+requestLevelOptionString(pt, 'Escolha um nível [~d-~d] ou digite 0').
+requestTryAgainString(pt, 'Tente Novamente').
+indicatePlayerTurnString(pt, 'Turno do jogador ~s').
+indicateInvalidPieceString(pt, 'Essa peça não é sua').
+indicateNoMovesString(pt, 'Não existem capturas disponíveis, remova uma peça').
+indicateInvalidDomainString(pt, 'O valor deve pertencer ao intervalo [~w-~w]').
+indicateNotMemberString(pt, 'O valor deve ser um de ~w').
