7a377ad226b76728a4ba7434d429ba392a56fe38
a3brx <a3brx@sapo.pt>
2020-11-28 16:21:27 +0000

    end

--- lib/text/victory.pl
+++ lib/text/victory.pl
@@ -1,4 +1,13 @@
-display_x_wins :-
+:- consult('../variables.pl').
+
+print_winner(Winner) :-
+	language(Language),
+	display_winner(Language, Winner).
+
+% ----------------------------------------------------------------
+% English
+% ----------------------------------------------------------------
+display_winner(en, 1) :-
 	write('         _                                                       _                 _ '), nl,
 	write(' _ __   | |   __ _   _   _    ___   _ __    __  __   __      __ (_)  _ __    ___  | |'), nl,
 	write('| \'_ \\  | |  / _\` | | | | |  / _ \\ | \'__|   \\ \\/ /   \\ \\ /\\ / / | | | \'_ \\  / __| | |'), nl,
@@ -6,7 +15,7 @@ display_x_wins :-
 	write('| .__/  |_|  \\__,_|  \\__, |  \\___| |_|      /_/\\_\\     \\_/\\_/   |_| |_| |_| |___/ (_)'), nl,
 	write('|_|                  |___/                                                           '), nl.
 
-display_o_wins :-
+display_winner(en, 2) :-
 	write('          _                                    ___                 _                 _ '), nl,
 	write('  _ __   | |   __ _   _   _    ___   _ __     /   \\    __      __ (_)  _ __    ___  | |'), nl,
 	write(' | \'_ \\  | |  / _` | | | | |  / _ \\ | \'__|   |     |   \\ \\ /\\ / / | | | \'_ \\  / __| | |'), nl,
@@ -14,3 +23,6 @@ display_o_wins :-
 	write(' | .__/  |_|  \\__,_|  \\__, |  \\___| |_|       \\___/      \\_/\\_/   |_| |_| |_| |___/ (_)'), nl,
 	write(' |_|                  |___/                                                                  '), nl.
 
+% English is the default if there is no translation available
+display_winner(_, Winner) :-
+	display_winner(en, Winner).
